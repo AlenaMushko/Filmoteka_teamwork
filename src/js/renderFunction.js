02c32320d1393526619c5c0be385d7a970ddf41a
alena <evstafienko_a@ukr.net>
2023-01-12 01:35:35 +0200

    if genres>=4

--- src/js/renderFunction.js
+++ src/js/renderFunction.js
@@ -20,8 +20,8 @@ export function renderSliderFilmCard({ results }) {
                   <div class="glide__text">
                   <h2 class="glide__title">${title}</h2>
                   <p class="glide__genres">${filmGenreId}<span>|${(
-        release_date || '2022'
-      ).slice(0, 4)}</span></p></div></div></a>
+          release_date || '2022'
+        ).slice(0, 4)}</span></p></div></div></a>
               </li>`;
     })
     .join('');
@@ -31,12 +31,17 @@ export function renderSliderFilmCard({ results }) {
 export function renderFilmCard({ results }) {
   const markup = results
     .map(({ id, poster_path, title, release_date, genre_ids }) => {
-      let filmGenreId = '';
+      let filmGenre = '';
       if (genre_ids) {
-        filmGenreId = genresId
+        let filmGenreId = genresId
           .filter(({ id }) => genre_ids.includes(id))
-          .map(({ name }) => name)
-          .join(', ');
+          .map(({ name }) => name);
+
+        if (filmGenreId.length >= 4) {
+          filmGenre = `${filmGenreId.slice(0, 2).join(', ')},  Others`;
+        } else {
+          filmGenre = filmGenreId.join(', ');
+        }
       }
 
       const img = `<img   class='film__img' alt= '${title}' width='100%'
@@ -45,12 +50,11 @@ export function renderFilmCard({ results }) {
       return `<li class="film__item" data-id=${id}>
                   ${poster_path ? img : '<p>Poster is not available.</p>'}
                   <h2 class="films__title">${title}</h2>
-                  <p class="films__genres">${
-                    filmGenreId || 'Not available'
-                  }<span>|${(release_date || 'Not available').slice(
-        0,
-        4
-      )}</span></p>
+                  <p class="films__genres">${filmGenre || 'Not available'
+        }<span>|${(release_date || 'Not available').slice(
+          0,
+          4
+        )}</span></p>
               </li>`;
     })
     .join('');
