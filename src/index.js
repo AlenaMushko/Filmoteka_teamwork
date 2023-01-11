ce06c9f988ae2b7bf135a3a3001cc2dd58e017db
Alexandr Katyshev <katyshev1@gmail.com>
2023-01-11 12:28:09 +0200

    modal temolate html css and js done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { sliderRevenueFilms } from './js/slideRevenueFilms';
 import { backToTop } from './js/scrolToHome';
 import { ShowFilms } from './js/functionsForFilms';
+import { modalWindows } from './js/modal';
 
 sliderRevenueFilms();
 // слайлдер з фільмами, що зібрали найбільшу касу
@@ -10,3 +11,5 @@ ShowFilms();
 backToTop();
 // кнопка повернення до гори
 
+modalWindows();
+// модальні вікна
