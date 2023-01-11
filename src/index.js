032406a1d06e0a00a0c63d9317701a4ee7c778d9
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-01-11 19:42:44 +0200

    added html and style button on header

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import { ShowFilms } from './js/functionsForFilms';
 sliderRevenueFilms();
 // слайлдер з фільмами, що зібрали найбільшу касу
 ShowFilms();
-// фільми топ, фільми за пошуком 
+// фільми топ, фільми за пошуком
 
 backToTop();
 // кнопка повернення до гори
-
