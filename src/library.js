1da75502ff56965a14a40c01fc06b936c829639e
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2023-01-13 01:07:40 +0200

    footer modal on

--- src/library.js
+++ src/library.js
@@ -1,9 +1,12 @@
 import { sliderRevenueFilms } from './js/slideRevenueFilms';
 import { backToTop } from './js/scrolToHome';
-;
+import { onTeamModal } from './js/team_modal';
 
 sliderRevenueFilms();
 // слайлдер з фільмами, що зібрали найбільшу касу
 
 // backToTop();
 // // кнопка повернення до гори
+
+// footer
+onTeamModal();
