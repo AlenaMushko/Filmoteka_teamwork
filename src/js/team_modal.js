3f782fe01186aad26aa25db8bf512a5c7ab79187
Alexandr Nik <107132921+alexandr-nik@users.noreply.github.com>
2023-01-12 17:17:09 +0200

    footer add slider

--- src/js/team_modal.js
+++ src/js/team_modal.js
@@ -1,26 +1,52 @@
-import { refs } from './refs';
+
+import Glide from '@glidejs/glide';
+import { Modal } from './modal';
+const teamModalWindow = new Modal(
+  '.team-modal-open',
+  '.team-modal-close',
+  '.team-modal-overlay',
+  '.team-modal'
+);
+teamModalWindow.openBtn.addEventListener(
+  'click',
+  teamModalWindow.openModal.bind(teamModalWindow)
+);
+
+
+//slider
+const checkbox = document.querySelector('.options-bound-checkbox');
+console.log(checkbox);
+const teamSlider = new Glide('.teamSlider', {
+  type: 'carousel',
+  bound: checkbox.checked,
+  perView: 1,
+  autoplay: 2500,
+  keyboard: true,
+  focusAt: 'center',
+  hoverpause: true,
+  breakpoints: {
+    800: {
+      perView: 2,
+    },
+    480: {
+      perView: 1,
+    },
+  },
+});
+
+// teamSlider.mount();
+
+checkbox.addEventListener('change', function () {
+  teamSlider.update({
+    bound: checkbox.checked,
+  });
+});
 
 export function onTeamModal() {
-  refs.teamOpenModalBtn.addEventListener('click', openTeamModal);
-}
-
-function teamModalClose() {
-  refs.teamModal.classList.add('is-hidden');
-}
-function onEscapeClickTM(e) {
-  if (e.key === 'Escape') {
-    teamModalClose();
-    window.removeEventListener('keydown', onEscapeClickTM);
-  }
-}
-function openTeamModal() {
-  teamModalOpen();
-  refs.teamModalCloseBtn.addEventListener('click', teamModalClose);
-  window.addEventListener('keydown', onEscapeClickTM);
-}
-function teamModalOpen() {
-  refs.teamModal.classList.remove('is-hidden');
- 
- 
-
+  teamSlider.mount();
+  checkbox.addEventListener('change', function () {
+    teamSlider.update({
+      bound: checkbox.checked,
+    });
+  });
 }
