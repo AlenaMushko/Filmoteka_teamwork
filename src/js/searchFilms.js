e520d35d455cf7a3a378e3de49ff5093c192c168
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-01-12 18:20:16 +0200

    added paginationBackToTop

--- src/js/searchFilms.js
+++ src/js/searchFilms.js
@@ -4,6 +4,7 @@ import { renderFilmCard } from './renderFunction';
 import { refs } from './refs';
 import { pagination } from './pagination';
 import { cleanPagination } from './pagination';
+import { paginationBackToTop } from './pagination';
 
 const apiService = new ApiService();
 
@@ -35,6 +36,7 @@ export async function onSearchFormSubmit(e) {
     pagination.on('afterMove', loadMoreSearchFilms);
 
     async function loadMoreSearchFilms(event) {
+      paginationBackToTop();
       const currentPage = event.page;
       apiService.pageNum = currentPage;
       const results = await apiService.getSearchFilms();
