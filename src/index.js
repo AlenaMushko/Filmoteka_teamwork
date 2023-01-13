ad32e491783adbfbcdfe08cb956a922e8683120a
alena <evstafienko_a@ukr.net>
2023-01-13 04:02:28 +0200

    add filmCardModalWindow in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { scrolToTop } from './js/scrolToTop';
 import { ShowFilms } from './js/functionsForFilms';
 import { onTeamModal } from './js/team_modal';
 import { registrationModalOpen } from './js/modalRegistrationFunction';
+import { filmCardModalWindow } from './js/modal-film';
 import 'lazysizes';
 // import a plugin
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
@@ -16,6 +17,10 @@ sliderRevenueFilms();
 // слайлдер з фільмами, що зібрали найбільшу касу
 ShowFilms();
 // фільми топ, фільми за пошуком
+
+filmCardModalWindow();
+// модалка карточки фільму
+
 scrolToTop();
 // кнопка повернення до гори
 
