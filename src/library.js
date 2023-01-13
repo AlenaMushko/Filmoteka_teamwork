4afb17731a726654e6b34af224d2d8ae19a6623f
alena <evstafienko_a@ukr.net>
2023-01-13 02:45:55 +0200

    add lazysizes library

--- src/library.js
+++ src/library.js
@@ -1,11 +1,11 @@
 import { sliderRevenueFilms } from './js/slideRevenueFilms';
-// import { scrolToTop } from './js/scrolToTop';
+import { scrolToTop } from './js/scrolToTop';
 import { onTeamModal } from './js/team_modal';
 
 sliderRevenueFilms();
 // слайлдер з фільмами, що зібрали найбільшу касу
 
-// scrolToTop();
+scrolToTop();
 // кнопка повернення до гори
 
 // footer
