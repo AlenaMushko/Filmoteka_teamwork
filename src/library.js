6e78e1f43eec65ebc8cefafa993cd9fa28978acf
alena <evstafienko_a@ukr.net>
2023-01-15 16:24:49 +0200

    v

--- src/library.js
+++ src/library.js
@@ -43,39 +43,86 @@ onTeamModal();
 
 // ---------------------------------
 
-// import { MyLibrary } from './js/localStorage';
+import { MyLibrary } from './js/localStorage';
 
-// import { renderFilmCard } from './js/renderFunction';
-// import axios from 'axios';
-// import genresId from './genres.json';
-// import { refs } from './js/refs';
+import { renderFilmCard } from './js/renderFunction';
+import axios from 'axios';
+import genresId from './genres.json';
+import { refs } from './js/refs';
 
-// const myLibrary = new MyLibrary();
-// const KEY = '32432509d17cea42104bbb7507a382c7';
-// const api_key = `?api_key=${KEY}`;
-// const BASE_URL = 'https://api.themoviedb.org/3/';
+const myLibrary = new MyLibrary();
+const KEY = '32432509d17cea42104bbb7507a382c7';
+const api_key = `?api_key=${KEY}`;
+const BASE_URL = 'https://api.themoviedb.org/3/';
 
-// let arrWatchedFilms = myLibrary.getFromQueue();
-// // let arrWatchedFilms = myLibrary.getFromWatched();
-// console.log(arrWatchedFilms);
+let arrWatchedFilms = myLibrary.getFromQueue();
+// let arrWatchedFilms = myLibrary.getFromWatched();
+console.log(arrWatchedFilms);
 
-// async function getFilmFromLocalStorage(arrWatchedFilms) {
-//   const data = await Promise.all(
-//     arrWatchedFilms.map(idWatchedFilm => {
-//       try {
-//         const url = `${BASE_URL}movie/${idWatchedFilm}${api_key}&append_to_response=images`;
-//         return axios.get(url).then(response => {
-//           if (!response) {
-//             throw new Error(response.status);
-//           }
-//           return response.data;
-//         });
-//       } catch (error) {
-//         console.error();
-//       }
-//     }));
-//   return data;
-// };
+async function getFilmFromLocalStorage(arrWatchedFilms) {
+  const data = await Promise.all(
+    arrWatchedFilms.map(idWatchedFilm => {
+      try {
+        const url = `${BASE_URL}movie/${idWatchedFilm}${api_key}&append_to_response=images`;
+        return axios.get(url).then(response => {
+          if (!response) {
+            throw new Error(response.status);
+          }
+          return response.data;
+        });
+      } catch (error) {
+        console.error();
+      }
+    }));
+  return data;
+};
+
+renderWatchedFilmInLibrary();
+async function renderWatchedFilmInLibrary() {
+  const filmInfo = await getFilmFromLocalStorage(arrWatchedFilms);
+  try {
+    console.log(...filmInfo);
+    renderFilmCard(...filmInfo);
+    // renderFilmCardLibrary(filmInfo)
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// renderFilmCardLibrary();
+// function renderFilmCardLibrary(arrWatchedFilms) {
+//   const markup = arrWatchedFilms.map(film => {
+//     cardFilmLibrary(film).join('');})
+// // refs.movieLibrary.innerHTML = markup;
+//   // console.log(markup);
+//   refs.movieLibrary.insertAdjacentHTML('afterbegin', markup);
+//   }
+
+function cardFilmLibrary({ id, poster_path, title, original_title, original_name, release_date, first_air_date, genres, vote_average }) {
+  const filmGenre = genres.slice(0, 3).map(({ name }) => name)
+    .join(', ');
+const voteAverage = Number(vote_average).toFixed(1) 
+const foto = `<img   class='film__img lazyload' alt= '${title || original_title || original_name}' width='100%' loading="lazy"
+      src='./images/poster_photo.png'/>`
+const img = `<img   class='film__img lazyload' alt= '${title || original_title || original_name}' width='100%' loading="lazy"
+      data-src='https://image.tmdb.org/t/p/original${poster_path}'/>`;
+const imgPlug = `<img  class="film__img" '${title || original_title || original_name}' width='100%' 
+       src= '${foto}'`;
+
+return`<li class="film__item" data-id=${id}>
+                  ${poster_path ? img : foto}
+                  <h2 class="films__title">${original_title || title || original_name} </h2>
+                  <p class="films__genres">${filmGenre || 'Not available'
+  }<span>|${(release_date || first_air_date || 'Not available').slice(
+    0,
+    4
+  )}</span></p>
+      <p class="films__voteaverage">${voteAverage}</p>
+              </li>`
+}
+
+
 
 // renderWatchedFilmInLibrary();
 // async function renderWatchedFilmInLibrary() {
@@ -89,34 +136,3 @@ onTeamModal();
 //     console.log(error);
 //   }
 // }
-
-// function renderFilmCardLibrary({ id, poster_path, title, original_title, original_name, release_date, first_air_date, genres, vote_average }) {
-//   const filmGenre = genres.slice(0, 3).map(({ name }) => name)
-//     .join(', ');
-// const voteAverage = Number(vote_average).toFixed(1) 
-// const foto = './images/poster_photo.png';
-// const img = `<img   class='film__img lazyload' alt= '${title || original_title || original_name}' width='100%' loading="lazy"
-//       data-src='https://image.tmdb.org/t/p/original${poster_path}'/>`;
-// const imgPlug = `<img  class="film__img" '${title || original_title || original_name}' width='100%' 
-//        src= '${foto}'`;
-
-// const markup = `<li class="film__item" data-id=${id}>
-//                   ${poster_path ? img : foto}
-//                   <h2 class="films__title">${original_title || title || original_name} </h2>
-//                   <p class="films__genres">${filmGenre || 'Not available'
-//   }<span>|${(release_date || first_air_date || 'Not available').slice(
-//     0,
-//     4
-//   )}</span></p>
-//       <p class="films__voteaverage">${voteAverage}</p>
-//               </li>`;
-  
-  
-// // refs.movieLibrary.innerHTML = markup;
-//   // console.log(markup);
-//   refs.movieLibrary.insertAdjacentHTML('afterbegin', markup);
-// }
-
-
-
-
