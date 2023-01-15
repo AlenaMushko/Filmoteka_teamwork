37691921c38955f89420b796a45a0d88252ad787
alena <evstafienko_a@ukr.net>
2023-01-15 09:02:52 +0200

    render films in library

--- src/library.js
+++ src/library.js
@@ -43,82 +43,77 @@ onFirstLoadTheme();
 // ---------------------------------
 
 
-import MyLibrary from './js/localStorage';
-import { renderFilmCard } from './js/renderFunction';
-import axios from 'axios';
-import genresId from './genres.json';
+// import MyLibrary from './js/localStorage';
+// import { renderFilmCard } from './js/renderFunction';
+// import axios from 'axios';
+// import genresId from './genres.json';
+// import { refs } from './js/refs';
 
+// const myLibrary = new MyLibrary();
+// const KEY = '32432509d17cea42104bbb7507a382c7';
+// const api_key = `?api_key=${KEY}`;
+// const BASE_URL = 'https://api.themoviedb.org/3/';
 
-const myLibrary = new MyLibrary();
-const KEY = '32432509d17cea42104bbb7507a382c7';
-const api_key = `?api_key=${KEY}`;
-const BASE_URL = 'https://api.themoviedb.org/3/';
+// async function getFilmFromLocalStorage(arrWatchedFilms) {
+//     Promise.all(arrWatchedFilms.map((idWatchedFilm) => {
+//       try {
+//         const url = `${BASE_URL}movie/${idWatchedFilm}${api_key}&append_to_response=images`;
+//         return axios.get(url).then(response => {
+//           if (!response) {
+//             throw new Error(response.status);
+//           }
+//           // console.log(response.data);
+//           return response.data;
+//         });
+//       } catch (error) {
+//         console.error();
+//       }
+//     }))
+// };
 
-async function getFilmFromLocalStorage(arrWatchedFilms) {
-    Promise.all(arrWatchedFilms.map((idWatchedFilm) => {
-      try {
-        const url = `${BASE_URL}movie/${idWatchedFilm}${api_key}&append_to_response=images`;
-        return axios.get(url).then(response => {
-          if (!response) {
-            throw new Error(response.status);
-          }
-          // console.log(response.data);
-          return response.data;
-        });
-      } catch (error) {
-        console.error();
-      }
-    }))
-};
+// function filmCardToLibrary( id, poster_path, title, original_title, original_name, release_date, first_air_date, genre_ids ) {
+//   let filmGenreId = '';
+//   if (genre_ids) {
+//     filmGenreId = genresId
+//       .filter(({ id }) => genre_ids.includes(id))
+//       .map(({ name }) => name)
+//       .join(', ');
+//   }
+//   return `
+//       <li class="glide__slide" data-id=${id}>
+//       <a class="glide__link" href= "">
+//       <div class="glide__container">
+//                  <img   class='glide__img' alt= '${title || original_title || original_name}' width='360' loading="lazy"
+//                   src='https://image.tmdb.org/t/p/original${poster_path}'/>
+//                   <div class="glide__text">
+//                   <h2 class="glide__title">${title || original_title || original_name}</h2>
+//                   <p class="glide__genres">${filmGenreId}<span>|${(
+//         release_date || first_air_date|| 'Not available'
+//       ).slice(0, 4)}</span></p></div></div></a>
+//               </li>`;
+// }
 
-function addFilmCardToLibrary( id, poster_path, title, original_title, original_name, release_date, first_air_date, genre_ids ) {
-  let filmGenreId = '';
-  if (genre_ids) {
-    filmGenreId = genresId
-      .filter(({ id }) => genre_ids.includes(id))
-      .map(({ name }) => name)
-      .join(', ');
-  }
-  return `
-      <li class="glide__slide" data-id=${id}>
-      <a class="glide__link" href= "">
-      <div class="glide__container">
-                 <img   class='glide__img' alt= '${title || original_title || original_name}' width='360' loading="lazy"
-                  src='https://image.tmdb.org/t/p/original${poster_path}'/>
-                  <div class="glide__text">
-                  <h2 class="glide__title">${title || original_title || original_name}</h2>
-                  <p class="glide__genres">${filmGenreId}<span>|${(
-        release_date || first_air_date|| 'Not available'
-      ).slice(0, 4)}</span></p></div></div></a>
-              </li>`;
-}
+// let arrWatchedFilms = myLibrary.getFromWatched();
 
-let arrWatchedFilms = myLibrary.getFromWatched();
-
-function renderCard() {
-  getFilmFromLocalStorage(arrWatchedFilms).then((film) => {
-    const markup = addFilmCardToLibrary(film);
-    refs.glideSlides.innerHTML = markup;
-    console.log(markup);
-    console.log(film);
-  })
-}
+// function renderCard(film) {
+//     const markup = filmCardToLibrary(film);
+//     refs.glideSlides.innerHTML = markup;
+//     console.log(markup);
+//     console.log(film);
+// }
 // renderWatchedFilmInLibrary()
- async function renderWatchedFilmInLibrary() {
-    const results = await getFilmFromLocalStorage(arrWatchedFilms);
-  console.log(results);
-  console.log(arrWatchedFilms);
-  try {
-    renderFilmCard(results);
-    pagination.reset(results.total_results);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// function renderWatchedFilmInLibrary() {
+//   getFilmFromLocalStorage(arrWatchedFilms).then(result => {
+//     console.log(result);
+//     renderCard(result);
+//    }).catch((error)=> {
+//     console.log(error);
+//   })
+// }
+
+// renderCard()
 
 
 
 
 
-
-renderCard()
