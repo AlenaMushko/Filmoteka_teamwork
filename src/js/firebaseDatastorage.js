027c6eaa67361097f4bcf1b0dcab1cf2dec97cd4
teiatan <92535482+teiatan@users.noreply.github.com>
2023-01-16 03:55:48 +0200

    rewrite auth to two functions

--- src/js/firebaseDatastorage.js
+++ src/js/firebaseDatastorage.js
@@ -1,5 +1,10 @@
 import { refs } from './refs';
-
+import { addNewUser } from './signUp';
+export function firebaseRealtimeDatabase() {
+  setInterval(deleteDataFromFirebaseStorage, 10000);
+  setInterval(giveLocalStorageToFirebaseStorage, 10000);
+  addNewUser();
+};
 class UserStorage {
 
     // функція, яка додає в базу даних новий запис 
@@ -10,7 +15,6 @@ class UserStorage {
         return fetch(`https://filmoteka-25bd4-default-rtdb.firebaseio.com/${localStorage.authId}.json`, {
           method: 'POST',
           body: JSON.stringify(newSaving),
-
         })
           .then(response => response.json())
           .then(response => {
@@ -18,7 +22,6 @@ class UserStorage {
             
         })
     };
-
     // функція, яка забирає з бази даних
     static take() {
         return fetch(`https://filmoteka-25bd4-default-rtdb.firebaseio.com/${localStorage.authId}.json?auth=${localStorage.authId}`)
@@ -34,7 +37,21 @@ class UserStorage {
             };
             location.reload();
         })
-    }
+    };
+
+    static delete(dataToDelete) {
+      const previousSavingId = localStorage.savingId;
+      return fetch(`https://filmoteka-25bd4-default-rtdb.firebaseio.com/${localStorage.authId}/${previousSavingId}.json?x-http-method-override=DELETE`, {
+        method: 'POST',
+        body: JSON.stringify(dataToDelete),
+
+      })
+        .then(response => response.json())
+        .then(response => {
+
+      })
+  };
+
 }
 
 // створення сховища для нового користувача на бекенді
@@ -43,15 +60,17 @@ export function giveLocalStorageToFirebaseStorage() {
     UserStorage.create(localStorage);
   };
 };
-
 // підтягування з бекенду сховища користувача, при авторизації
 export function takeLocalStorageFromFirebaseStorage() {
     UserStorage.take();
 };
-
 //зміна сховища користувача на бекенді 
 export function changeLocalStorageInFirebaseStorage() {
     if(localStorage.auth === "yes") {
         UserStorage.change(localStorage);
     };
 };
+
+export function deleteDataFromFirebaseStorage() {
+  UserStorage.delete();
+}
--- src/js/signUp.js
+++ src/js/signUp.js
@@ -3,7 +3,38 @@ import { refs } from './refs';
 
 const email = refs.authEmailInput;
 const password = refs.authPasswordInput;
-refs.authRegistrationBtn.addEventListener('click', signUpBtnHandler);
-export function signUpBtnHandler (email, password) {
-    
-}
+
+export function addNewUser() {
+    refs.authRegistrationBtn.addEventListener('click', signUpBtnHandler);
+    refs.authRegistrationBtn.removeAttribute('disabled');
+    refs.authRegistrationBtn.setAttribute('type', 'button');
+};
+
+function signUpBtnHandler(e) {
+    e.preventDefault;
+    const apiKey = 'AIzaSyCh4IOUhN3RY5RpYi3dFrDkgc69KqBpI3o';
+
+    fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email, password,
+        returnSecureToken: true
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.registered);
+        if (data.registered === true) {
+            console.log('2');
+            Notiflix.Report.failure(
+                'User with such data already exists',
+                'Please, sign in with your password or sign up with another mail',
+                'Okay',);
+            return;
+        }
+        console.log('3');
+    })     
+};
