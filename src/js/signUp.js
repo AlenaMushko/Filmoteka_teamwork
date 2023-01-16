ae4c69c5c81197e00d75461f31c6cb7f3031b732
teiatan <92535482+teiatan@users.noreply.github.com>
2023-01-16 02:36:14 +0200

    sugn up function

---
+++ src/js/signUp.js
@@ -0,0 +1,9 @@
+import { refs } from './refs';
+
+
+const email = refs.authEmailInput;
+const password = refs.authPasswordInput;
+refs.authRegistrationBtn.addEventListener('click', signUpBtnHandler);
+export function signUpBtnHandler (email, password) {
+    
+}
