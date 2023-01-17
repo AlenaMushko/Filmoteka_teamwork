c5a2b3f9c87575f98878449901555d70326b9869
Dmytro-1506 <kyunlik@gmail.com>
2023-01-17 20:57:57 +0100

    change buttons language

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -4,26 +4,26 @@ export class MyLibrary {
     }
 
     addToWatched() {
-        let newMovieID = getMovieID();
-        let localWatchedMovies = myLybrary.getFromWatched();
+        let newMovieID = myLibrary.getMovieID();
+        let localWatchedMovies = myLibrary.getFromWatched();
         if (!localWatchedMovies) {
             localWatchedMovies = [newMovieID]
             localStorage.setItem("watchedMovies", JSON.stringify(localWatchedMovies));
         }
     else if(localWatchedMovies.includes(newMovieID)){
-            return;
-        }else{
+        return;
+    }else{
         localWatchedMovies.push(newMovieID);
         localStorage.setItem("watchedMovies", JSON.stringify(localWatchedMovies));
-        }
+    }
     }
 
     removeFromWatched() {
-        let newMovieID = getMovieID();
+        let newMovieID = myLibrary.getMovieID();
         if (!newMovieID) {
             return;
         } else {
-            removeFromLocalStorage(myLybrary.getFromWatched, 'watchedMovies');
+            myLibrary.removeFromLocalStorage(myLibrary.getFromWatched, 'watchedMovies');
         }
     }
     
@@ -32,113 +32,135 @@ export class MyLibrary {
     }
 
     addToQueue() {
-        let newMovieID = getMovieID();
-        let localQueueList = myLybrary.getFromQueue();
+        let newMovieID = myLibrary.getMovieID();
+        let localQueueList = myLibrary.getFromQueue();
         if (!localQueueList) {
             localQueueList = [newMovieID]
             localStorage.setItem('queueMovies', JSON.stringify(localQueueList));
         }
     else if(localQueueList.includes(newMovieID)){
-            return;
-        }else{
+        return;
+    }else{
         localQueueList.push(newMovieID);
         localStorage.setItem('queueMovies', JSON.stringify(localQueueList));
-        }
+    }
     }
 
     removeFromQueue() {
-        let newMovieID = getMovieID();
+    let newMovieID = myLibrary.getMovieID();
         if (!newMovieID) {
             return;
         } else {
-            removeFromLocalStorage(myLybrary.getFromQueue, 'queueMovies');
+            myLibrary.removeFromLocalStorage(myLibrary.getFromQueue, 'queueMovies');
         }
     }
-}
-const myLybrary = new MyLibrary;
 
-function getMovieID() {
+    getMovieID() {
     const filmModalContent = document.querySelector('.film-modal__content');
     return filmModalContent.getAttribute('film-modal-id');
-}
+    }
 
-function removeFromLocalStorage(libraryArrey, lybraryName) {
+    removeFromLocalStorage(libraryArrey, lybraryName) {
     const getLibraryArrey = libraryArrey;
     const newLibraryArrey = getLibraryArrey();
-    const newMovieID = getMovieID();
+    const newMovieID = myLibrary.getMovieID();
     if(!newLibraryArrey.includes(newMovieID)){
         return;
     } else {
         const movieToRemoveIndex = newLibraryArrey.indexOf(newMovieID);
         newLibraryArrey.splice(movieToRemoveIndex, 1);
         localStorage.setItem(lybraryName, JSON.stringify(newLibraryArrey));
+        }
     }
 }
+const myLibrary = new MyLibrary;
 
-export function addEventListenerOnButtonaAddWatchedAndAddQueue() {
-    const authAddToWatched = document.querySelector('.button-watched');
-    const authAddToQueue = document.querySelector('.button-queue');
-    const openReg = document.querySelector('.navigation__open--btn');
+// function getMovieID() {
+//     const filmModalContent = document.querySelector('.film-modal__content');
+//     return filmModalContent.getAttribute('film-modal-id');
+// }
+
+// function removeFromLocalStorage(libraryArrey, lybraryName) {
+//     const getLibraryArrey = libraryArrey;
+//     const newLibraryArrey = getLibraryArrey();
+//     const newMovieID = getMovieID();
+//     if(!newLibraryArrey.includes(newMovieID)){
+//         return;
+//     } else {
+//         const movieToRemoveIndex = newLibraryArrey.indexOf(newMovieID);
+//         newLibraryArrey.splice(movieToRemoveIndex, 1);
+//         localStorage.setItem(lybraryName, JSON.stringify(newLibraryArrey));
+//     }
+// }
+// import { btnLibraryWatchedOrQueue } from './watched_queue';
+
+// export function addEventListenerOnButtonaAddWatchedAndAddQueue() {
+//     const authAddToWatched = document.querySelector('.button-watched');
+//     const authAddToQueue = document.querySelector('.button-queue');
+//     const openReg = document.querySelector('.navigation__open--btn');
     
-    checkAuthUser();
+//     checkAuthUser();
 
-    function checkAuthUser() {
-        if (localStorage.auth === 'no') {
-            authAddToQueue.addEventListener('click', () => {
-                openAuthModal();
-            });
-            authAddToWatched.addEventListener('click', () => {
-                openAuthModal();
-            });
-            return;
-        } else {
-            checkLocalMoviesList(myLybrary.getFromWatched, 'watched', authAddToWatched, myLybrary.removeFromWatched, myLybrary.addToWatched);
-            checkLocalMoviesList(myLybrary.getFromQueue, 'queue', authAddToQueue, myLybrary.removeFromQueue, myLybrary.addToQueue);
-        }
-    }
+//     function checkAuthUser() {
+//         if (localStorage.auth === 'no') {
+//             authAddToQueue.addEventListener('click', () => {
+//                 openAuthModal();
+//             });
+//             authAddToWatched.addEventListener('click', () => {
+//                 openAuthModal();
+//             });
+//             return;
+//         } else {
+//             checkLocalMoviesList(myLybrary.getFromWatched, 'watched', authAddToWatched, myLybrary.removeFromWatched, myLybrary.addToWatched);
+//             checkLocalMoviesList(myLybrary.getFromQueue, 'queue', authAddToQueue, myLybrary.removeFromQueue, myLybrary.addToQueue);
+//         }
+//     }
 
-    function openAuthModal() {
-        authAddToWatched.addEventListener('click', openReg.click());
-        authAddToQueue.addEventListener('click', openReg.click());
-        const overlayLogin = document.querySelector('.overlay__log-in');
-        overlayLogin.style.zIndex = '30';
-    }
+//     function openAuthModal() {
+//         authAddToWatched.addEventListener('click', openReg.click());
+//         authAddToQueue.addEventListener('click', openReg.click());
+//         const overlayLogin = document.querySelector('.overlay__log-in');
+//         overlayLogin.style.zIndex = '30';
+//     }
+    
+//     // btnLibraryWatchedOrQueue()
 
-    function addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc) {
-        btnName.textContent = `add to ${lybraryName}`;
-        const newAddFunction = addFunc;
-        function onClick() {
-            newAddFunction();
-            chengeBtnToRemove(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
-            btnName.removeEventListener('click', onClick);
-        };
-        btnName.addEventListener('click', onClick);
-    }
+//     function addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc) {
+//         btnName.textContent = `add to ${lybraryName}`;
+//         const newAddFunction = addFunc;
+//         function onClick() {
+//             newAddFunction();
+//             // chengeBtnToRemove(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
+//             btnLibraryWatchedOrQueue();
+//             btnName.removeEventListener('click', onClick);
+//         };
+//         btnName.addEventListener('click', onClick);
+//     }
+    
+//     function chengeBtnToRemove(libraryArrey, lybraryName, btnName, removeFunc, addFunc) {
+//         const newRemoveFunction = removeFunc;
+//         btnName.textContent = `Remove from ${lybraryName}`;
+//         function onClick() {
+//             newRemoveFunction();
+//             addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
+//             btnName.removeEventListener('click', onClick);
+//         };
+//         btnName.addEventListener('click', onClick);
+//     }
 
-    function chengeBtnToRemove(libraryArrey, lybraryName, btnName, removeFunc, addFunc) {
-        const newRemoveFunction = removeFunc;
-        btnName.textContent = `Remove from ${lybraryName}`;
-        function onClick() {
-            newRemoveFunction();
-            addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
-            btnName.removeEventListener('click', onClick);
-        };
-        btnName.addEventListener('click', onClick);
-    }
-
-    function checkLocalMoviesList(libraryArrey, lybraryName, btnName, removeFunc, addFunc) {
-        const newLibraryArrey = libraryArrey;
-        const newMovieID = getMovieID();
-        if (!newLibraryArrey()) {
-            addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
-            return;
-        }
-        else if (newLibraryArrey().includes(newMovieID)) {
-            chengeBtnToRemove(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
-            return;
-        }
-        else {
-            addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
-        }
-    }
-}
+//     function checkLocalMoviesList(libraryArrey, lybraryName, btnName, removeFunc, addFunc) {
+//         const newLibraryArrey = libraryArrey;
+//         const newMovieID = getMovieID();
+//         if (!newLibraryArrey()) {
+//             addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
+//             return;
+//         }
+//         else if (newLibraryArrey().includes(newMovieID)) {
+//             chengeBtnToRemove(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
+//             return;
+//         }
+//         else {
+//             addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
+//         }
+//     }
+// }
---
+++ src/js/localStorageMovieManipulate.js
@@ -0,0 +1,84 @@
+import { MyLibrary } from './localStorage';
+import { langArr } from './translation';
+import { btnLibraryWatchedOrQueue } from './watched_queue';
+
+const myLibrary = new MyLibrary();
+
+export function addEventListenerOnButtonaAddWatchedAndAddQueue() {
+    const authAddToWatched = document.querySelector('.button-watched');
+    const authAddToQueue = document.querySelector('.button-queue');
+    const openReg = document.querySelector('.navigation__open--btn');
+    
+    checkAuthUser();
+
+    function changeBtnName(btnAction, lybraryName) {
+        const currentLang = localStorage.getItem('language');
+        const newBtnName = langArr[`${btnAction}-${lybraryName}`][currentLang];
+        return newBtnName
+    }
+
+    function checkAuthUser() {
+        if (localStorage.auth === 'no') {
+            authAddToQueue.addEventListener('click', () => {
+                openAuthModal();
+            });
+            authAddToWatched.addEventListener('click', () => {
+                openAuthModal();
+            });
+            return;
+        } else {
+            checkLocalMoviesList(myLibrary.getFromWatched, 'watched', authAddToWatched, myLibrary.removeFromWatched, myLibrary.addToWatched);
+            checkLocalMoviesList(myLibrary.getFromQueue, 'queue', authAddToQueue, myLibrary.removeFromQueue, myLibrary.addToQueue);
+        }
+    }
+
+    function openAuthModal() {
+        authAddToWatched.addEventListener('click', openReg.click());
+        authAddToQueue.addEventListener('click', openReg.click());
+        const overlayLogin = document.querySelector('.overlay__log-in');
+        overlayLogin.style.zIndex = '30';
+    }
+
+    function addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc) {
+        const newAddFunction = addFunc;
+        const btnAction = 'add';
+        const newBtnName = changeBtnName(btnAction, lybraryName);
+        btnName.textContent = newBtnName;
+        function onClick() {
+            newAddFunction();
+            chengeBtnToRemove(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
+            btnName.removeEventListener('click', onClick);
+        };
+        btnName.addEventListener('click', onClick);
+    }
+    
+    function chengeBtnToRemove(libraryArrey, lybraryName, btnName, removeFunc, addFunc) {
+        const newRemoveFunction = removeFunc;
+        const btnAction = 'remove';
+        const newBtnName = changeBtnName(btnAction, lybraryName);
+        btnName.textContent = newBtnName;
+        function onClick() {
+            newRemoveFunction();
+            btnLibraryWatchedOrQueue();
+            addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
+            btnName.removeEventListener('click', onClick);
+        };
+        btnName.addEventListener('click', onClick);
+    }
+
+    function checkLocalMoviesList(libraryArrey, lybraryName, btnName, removeFunc, addFunc) {
+        const newLibraryArrey = libraryArrey;
+        const newMovieID = myLibrary.getMovieID();
+        if (!newLibraryArrey()) {
+            addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
+            return;
+        }
+        else if (newLibraryArrey().includes(newMovieID)) {
+            chengeBtnToRemove(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
+            return;
+        }
+        else {
+            addMovieToLocalStorage(libraryArrey, lybraryName, btnName, removeFunc, addFunc);
+        }
+    }
+}
